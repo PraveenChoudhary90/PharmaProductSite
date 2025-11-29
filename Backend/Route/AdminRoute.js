90fd536ebc17321d9a37aa8502705b2722fb58b8
Praveen Choudhary <pc852180@gmail.com>
2025-11-29 22:16:05 +0530

    Postman

--- Backend/Route/AdminRoute.js
+++ Backend/Route/AdminRoute.js
@@ -1,10 +1,17 @@
 const express =require("express");
 const route = express.Router();
 const AdminContoller = require("../Controller/AdminController");
+const auth = require("../MiddleWare/auth");
+const AdminModel = require("../Model/AdminModel");
 
 route.post("/admininsert",AdminContoller.AdminInsert);
 route.post("/adminlogin",AdminContoller.AdminLogin);
 
+
+route.get("/admin/profile", auth, async (req, res) => {
+    const admin = await AdminModel.findById(req.admin.id);
+    res.json(admin);
+});
 
 
 
